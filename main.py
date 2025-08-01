738f9c407b4ea55c20f5a169638a357489d9f128
Kurlgargyey <nahsirkivar@gmail.com>
2025-08-01 08:51:55 +0200

    1-5: message history base

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     if len(sys.argv) == 1:
         print("Please provide a prompt...")
         exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     prompt = sys.argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
