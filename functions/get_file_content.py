a00eeba0cb6b01fdec12dbf4afc360a356e5591e
Kurlgargyey <nahsirkivar@gmail.com>
2025-08-01 09:54:05 +0200

    2-3 (WIP): work on getting contents of a file

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,5 +5,12 @@ def get_file_content(working_directory, file_path):
 
     if not os.path.isfile(file_path):
         error = f'Error: File not found or is not a regular file: "{file_path}"'
-        result += file_path
+        result += error
         return result
+    if not os.path.commonpath(working_directory, file_path) == working_directory:
+        error = f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        result += error
+        return result
+    MAX_CHARS = 10000
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
