deada7401bd1c68e36ceb5615f96981c9e484070
Kurlgargyey <nahsirkivar@gmail.com>
2025-08-01 08:55:28 +0200

    1-6: verbose mode

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
         print("Please provide a prompt...")
         exit(1)
 
+    verbose = "--verbose" in sys.argv
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -17,7 +18,9 @@ def main():
 
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 if __name__ == "__main__":
     main()
